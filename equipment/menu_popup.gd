f98e68676a28a3f954491a90b813720e0ee5de1b
NitroxNova <nitroxnova.games@gmail.com>
2025-01-18 23:56:15 -0600

    just use text box for material default and override, may want to set to a material not included in the input folder

--- equipment/menu_popup.gd
+++ equipment/menu_popup.gd
@@ -84,38 +84,6 @@ func fill_bone_options(idx:int):
 	var skeleton_data = HumanizerRigService.init_skeleton_data(rig,retargeted)
 	for bone_name in skeleton_data:
 		%BoneOptions.add_item(bone_name)
-
-func fill_material_options():
-	var options :OptionButton = %DefaultMaterial
-	options.clear()
-	options.add_item(" -- None (Random) --")
-	options.set_item_metadata(0,"")
-	var mat_list = HumanizerMaterialImportService.search_for_materials(get_mhclo_path())
-	mat_list = mat_list.materials
-	#print(mat_list)
-	for mat_id in mat_list:
-		#var mat_res = HumanizerResourceService.load_resource(mat_list[mat_id])
-		var mat_name = mat_id # because materials wont have been loaded yet
-		var idx = options.item_count
-		options.add_item(mat_name)
-		options.set_item_metadata(idx,mat_id)
-	
-	for idx in options.get_item_count():
-		var mat_id = options.get_item_metadata(idx)
-		if mat_id == import_settings.default_material:
-			options.selected = idx
-
-func fill_material_override_options():
-	var options: OptionButton = %MaterialOverride
-	options.clear()
-	options.add_item("")
-	var file_list = OSPath.get_files_recursive("res://data/input/equipment")
-	for file_name in file_list:
-		if file_name.get_extension() == "mhclo":
-			var equip_id = file_name.get_file().get_basename()
-			options.add_item(equip_id)
-			if import_settings.material_override == equip_id:
-				options.selected = options.item_count - 1
 		
 func fill_options(mhclo_path:String=""):
 	#print("fill options")
@@ -129,8 +97,10 @@ func fill_options(mhclo_path:String=""):
 	%GLB_Label.text = ""
 	%LoadRiggedGLB.current_dir = mhclo_path.get_base_dir()
 	import_settings = HumanizerEquipmentImportService.load_import_settings(mhclo_path)
-	fill_material_options()
-	fill_material_override_options()
+
+	%DefaultMaterial.text = import_settings.default_material
+	%MaterialOverride.text = import_settings.material_override
+	
 	var folder_override = HumanizerGlobalConfig.config.get_folder_override_slots(mhclo_path)
 	
 	if folder_override.is_empty():
@@ -168,8 +138,9 @@ func import_asset():
 	var string_id = import_settings.mhclo.get_basename().get_file()
 	if import_settings.display_name.strip_edges() == "":
 		printerr("No display name set, using string ID " + string_id) 
-	var select_material = %DefaultMaterial.selected
-	import_settings.default_material = %DefaultMaterial.get_item_metadata(select_material)
+
+	import_settings.default_material = %DefaultMaterial.text
+	import_settings.material_override = %MaterialOverride.text
 	import_settings.rigged_glb = %GLB_Label.text
 	import_settings.attach_bones = []
 	for hbox in %BoneList.get_children():
