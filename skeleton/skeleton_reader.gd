943be9c3ef18c3ab6d9970df6582d6afd805e06c
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2025-06-20 01:49:27 -0500

    convert from blender position/roll to godot rotation quaternion

--- skeleton/skeleton_reader.gd
+++ skeleton/skeleton_reader.gd
@@ -72,15 +72,25 @@ func load_bone_config():
 			var parent_id = skeleton.find_bone(parent_name)
 			rig.config[bone_id] = contents[in_name]
 			rig.config[bone_id].parent = parent_id
-			if rig.config[bone_id].head.strategy == "CUBE":
-				var cube_range = vertex_groups[rig.config[bone_id].head.cube_name][0]
-				var cube_index = []
-				for i in range(cube_range[0], cube_range[1] + 1):
-					cube_index.append(i)
-				rig.config[bone_id].head.vertex_indices = cube_index
-			
+			rig.config[bone_id].head.vertex_indices = get_vertex_indices(rig.config[bone_id].head)
+			rig.config[bone_id].tail.vertex_indices = get_vertex_indices(rig.config[bone_id].tail)
 			#rig.config_json_path = dir.path_join('skeleton_config.json')
 			#HumanizerResourceService.save_resource(rig.config_json_path, rig_config)
+
+func get_vertex_indices(config:Dictionary):
+	var vertex_indices = []
+	if config.strategy == "CUBE":
+		var cube_range = vertex_groups[config.cube_name][0]
+		var cube_index = []
+		for i in range(cube_range[0], cube_range[1] + 1):
+			cube_index.append(i)
+		vertex_indices = cube_index
+	elif config.strategy == "MEAN":
+		vertex_indices = config.vertex_indices
+	else:
+		vertex_indices.append(config.vertex_index)
+	return vertex_indices
+	
 
 func load_bone_weights():
 	# Get bone weights for clothes
@@ -168,7 +178,8 @@ func retarget_bone_names():
 		#might need better handling for default with toes. probably shouldnt have toe bones in game anyway...
 		retarget_names[side+"Toes"] = get_child_bone_with_pattern_recursive(retarget_names[side+"Foot"],["toe","ball"],side.to_lower(),1) 
 		#arms
-		retarget_names[side+"Shoulder"] = get_child_bone_with_pattern(retarget_names["UpperChest"],["shoulder","clavicle"],side.to_lower())
+		#retarget_names[side+"Shoulder"] = get_child_bone_with_pattern(retarget_names["UpperChest"],["shoulder","clavicle"],side.to_lower())
+		retarget_names[side+"Shoulder"] = get_child_bone_with_pattern(retarget_names["UpperChest"],["shoulder"],side.to_lower())
 		#default rig has clavicle and shoulder, using clavicle as shoulder since its closer to mixamo, but may change
 		retarget_names[side+"UpperArm"] = get_child_bone_with_pattern_recursive(retarget_names[side+"Shoulder"],["arm"],side.to_lower(),2) 
 		retarget_names[side+"LowerArm"] = get_child_bone_with_pattern_recursive(retarget_names[side+"UpperArm"],["forearm","lowerarm"],side.to_lower(),2) 
@@ -196,7 +207,7 @@ func retarget_bone_names():
 		else:
 			printerr("Unexpected Number of Fingers - " + str(finger_count))	
 		
-	print(retarget_names)
+	#print(retarget_names)
 
 func parse_finger(finger_name:String,side:String,pattern:Array):
 	retarget_names[side+finger_name+"Proximal"] = get_child_bone_with_pattern_recursive(retarget_names[side+"Hand"],pattern,side.to_lower(),2)
@@ -260,3 +271,389 @@ func get_bone_stack(bone_id:int,contains:Array,side:String,list:Array=[]):
 			list.append(child_id)
 			get_bone_stack(child_id,contains,side,list)
 	return list
+	
+static func get_bone_quat_from_position_roll(position1:Vector3,position2:Vector3,roll:float):
+	var bone_vector = position2 - position1
+	var rMatrix = Skeleton_Reader.vec_roll_to_mat3(bone_vector,0)
+	#print(rMatrix)
+	#var xform_matrix = Skeleton_Reader.rot_pos_to_m4(rMatrix,position1)
+	rMatrix = Skeleton_Reader.swizzle_rotation_matrix_y_up(rMatrix)
+	##xform_matrix = Skeleton_Reader.mul_m4db_m4db_m4fl(axis_basis_change,xform_matrix)
+	print(rMatrix)
+	#var decompose = Skeleton_Reader.decompose_matrix4(xform_matrix)
+	##print(decompose.rotation_m3[2][1])
+	#rMatrix = decompose.rotation_m3
+	#var r_basis = Basis(Vector3(rMatrix[0][0],rMatrix[0][1],rMatrix[0][2]),Vector3(rMatrix[1][0],rMatrix[1][1],rMatrix[1][2]),Vector3(rMatrix[2][0],rMatrix[2][1],rMatrix[2][2]))
+	#var r_quat = r_basis.get_rotation_quaternion()
+	var rotation_quaternion = Skeleton_Reader.mat3_normalized_to_quat_fast( rMatrix);
+	#rotate for up
+	rotation_quaternion = [rotation_quaternion[1],rotation_quaternion[3],-rotation_quaternion[2],rotation_quaternion[0]]
+	return rotation_quaternion
+	#$%rotation_label.text = str(rotation_quaternion)
+
+#func vec_roll_to_mat3_normalized(const float nor[3], const float roll, float r_mat[3][3])->void:
+#https://github.com/blender/blender/blob/1fbd83f72eaeffb08c080c938c89b749ef3a94d7/source/blender/blenkernel/intern/armature.cc#L2593
+static func vec_roll_to_mat3_normalized(nor:Vector3, roll:float):
+	#/**
+	#* P.S. In the end, this basically is a heavily optimized version of Damped Track +Y.
+	#*/
+	print("normal " + str(nor))
+	const SAFE_THRESHOLD:float = 6.1e-3;     #/* Theta above this value has good enough precision. */
+	const CRITICAL_THRESHOLD:float = 2.5e-4; #/* True singularity if XZ distance is below this. */
+	const THRESHOLD_SQUARED:float = CRITICAL_THRESHOLD * CRITICAL_THRESHOLD;
+
+	var x:float = nor[0];
+	var y:float = nor[1];
+	var z:float = nor[2];
+
+	var theta:float = 1.0 + y;                #/* Remapping Y from [-1,+1] to [0,2]. */
+	var theta_alt:float = x * x + z * z; #/* Squared distance from origin in x,z plane. */
+	var bMatrix = [[0,0,0],[0,0,0],[0,0,0]]
+	var rMatrix = [[0,0,0],[0,0,0],[0,0,0]]
+	#float rMatrix[3][3], bMatrix[3][3];
+	
+	#assert that its a valid vector?
+	#BLI_ASSERT_UNIT_V3(nor);
+
+	#/* Determine if the input is far enough from the true singularity of this type of
+	#* transformation at (0,-1,0), where roll becomes 0/0 undefined without a limit.
+	#*
+	#* When theta is close to zero (nor is aligned close to negative Y Axis),
+	#* we have to check we do have non-null X/Z components as well.
+	#* Also, due to float precision errors, nor can be (0.0, -0.99999994, 0.0) which results
+	#* in theta being close to zero. This will cause problems when theta is used as divisor.
+	#*/
+	if (theta > SAFE_THRESHOLD || theta_alt > THRESHOLD_SQUARED) :
+		#/* nor is *not* aligned to negative Y-axis (0,-1,0). */
+
+		bMatrix[0][1] = -x;
+		bMatrix[1][0] = x;
+		bMatrix[1][1] = y;
+		bMatrix[1][2] = z;
+		bMatrix[2][1] = -z;
+
+		if (theta <= SAFE_THRESHOLD) :
+		#/* When nor is close to negative Y axis (0,-1,0) the theta precision is very bad,
+		#* so recompute it from x and z instead, using the series expansion for `sqrt`. */
+			theta = theta_alt * 0.5 + theta_alt * theta_alt * 0.125;
+		#}
+#
+		bMatrix[0][0] = 1 - x * x / theta;
+		bMatrix[2][2] = 1 - z * z / theta;
+		bMatrix[2][0] = -x * z / theta;
+		bMatrix[0][2] = bMatrix[2][0]
+		#print(bMatrix)
+	#}
+	else:
+		printerr("nor is very close to negative Y axis (0,-1,0): use simple symmetry by Z axis.")
+		#/* nor is very close to negative Y axis (0,-1,0): use simple symmetry by Z axis. */
+		#unit_m3(bMatrix);
+		#bMatrix[0][0] =  -1.0;
+		#bMatrix[1][1] = bMatrix[0][0]
+	#}
+#
+	#/* Make Roll matrix */
+	axis_angle_normalized_to_mat3(rMatrix, nor, roll)
+#
+	#/* Combine and output result */
+	var r_mat = mul_m3_m3(rMatrix, bMatrix);
+	print("rMatrix")
+	print(rMatrix)
+	print("bMatrix")
+	print(bMatrix)
+	print("r_mat")
+	print(r_mat)
+	return r_mat
+	#}
+#
+#void axis_angle_normalized_to_mat3(float R[3][3], const float axis[3], const float angle)
+static func axis_angle_normalized_to_mat3(R:Array, axis:Vector3, angle:float):
+	#print("axis angle normalized to mat3")
+	axis_angle_normalized_to_mat3_ex(R, axis, sin(angle), cos(angle));
+#oid axis_angle_normalized_to_mat3_ex(float mat[3][3], const float axis[3], const float angle_sin, const float angle_cos
+static func axis_angle_normalized_to_mat3_ex(mat:Array, axis:Vector3, angle_sin:float, angle_cos:float):
+	var nsi:Vector3 
+	var ico:float;
+	var n_00:float
+	var n_01:float
+	var n_11:float
+	var n_02:float
+	var n_12:float
+	var n_22:float
+
+	#BLI_ASSERT_UNIT_V3(axis);
+
+	#/* now convert this to a 3x3 matrix */
+
+	ico = (1.0 - angle_cos);
+	nsi[0] = axis[0] * angle_sin;
+	nsi[1] = axis[1] * angle_sin;
+	nsi[2] = axis[2] * angle_sin;
+
+	n_00 = (axis[0] * axis[0]) * ico;
+	n_01 = (axis[0] * axis[1]) * ico;
+	n_11 = (axis[1] * axis[1]) * ico;
+	n_02 = (axis[0] * axis[2]) * ico;
+	n_12 = (axis[1] * axis[2]) * ico;
+	n_22 = (axis[2] * axis[2]) * ico;
+
+	mat[0][0] = n_00 + angle_cos;
+	mat[0][1] = n_01 + nsi[2];
+	mat[0][2] = n_02 - nsi[1];
+	mat[1][0] = n_01 - nsi[2];
+	mat[1][1] = n_11 + angle_cos;
+	mat[1][2] = n_12 + nsi[0];
+	mat[2][0] = n_02 + nsi[1];
+	mat[2][1] = n_12 - nsi[0];
+	mat[2][2] = n_22 + angle_cos;
+
+static func mul_m3_m3(A:Array, B:Array):
+	var R = [Vector3.ZERO,Vector3.ZERO,Vector3.ZERO]
+	R[0][0] = B[0][0] * A[0][0] + B[0][1] * A[1][0] + B[0][2] * A[2][0];
+	R[0][1] = B[0][0] * A[0][1] + B[0][1] * A[1][1] + B[0][2] * A[2][1];
+	R[0][2] = B[0][0] * A[0][2] + B[0][1] * A[1][2] + B[0][2] * A[2][2];
+
+	R[1][0] = B[1][0] * A[0][0] + B[1][1] * A[1][0] + B[1][2] * A[2][0];
+	R[1][1] = B[1][0] * A[0][1] + B[1][1] * A[1][1] + B[1][2] * A[2][1];
+	R[1][2] = B[1][0] * A[0][2] + B[1][1] * A[1][2] + B[1][2] * A[2][2];
+
+	R[2][0] = B[2][0] * A[0][0] + B[2][1] * A[1][0] + B[2][2] * A[2][0];
+	R[2][1] = B[2][0] * A[0][1] + B[2][1] * A[1][1] + B[2][2] * A[2][1];
+	R[2][2] = B[2][0] * A[0][2] + B[2][1] * A[1][2] + B[2][2] * A[2][2];
+	return R
+
+static func swizzle_rotation_matrix_y_up(input:Array):
+	#idk how matrix multiplication works. flip the 2nd and third row, and make the second row negative
+	var output = [Vector3.ZERO,Vector3.ZERO,Vector3.ZERO]
+	output[0] = input[0]
+	output[1] = input[2] * -1
+	output[2] = input[1]
+	return output
+
+static func decompose_matrix4(matrix:Array):
+	var position : Vector3
+	var rotation_m3 : Array = [Vector3.ZERO,Vector3.ZERO,Vector3.ZERO]
+	var rotation_quaternion 
+	var scale = Vector3.ZERO
+	
+	#mat4_to_loc_rot_size(loc, rot, size, (const float(*)[4])self->matrix);
+	   #float mat3[3][3]; /* wmat -> 3x3 */
+	   #copy_m3_m4(mat3, wmat);
+	var mat3 = make_m3_from_m4(matrix)
+	mat3_to_rot_size(rotation_m3, scale, mat3);
+	   #/* location */
+	   #copy_v3_v3(loc, wmat[3]);
+	# i dont know why we invert the matrix here, cant find it in the blender source
+	#rotation_m3 = [Vector3(rotation_m3[0][0],rotation_m3[1][0],rotation_m3[2][0]),Vector3(rotation_m3[0][1],rotation_m3[1][1],rotation_m3[2][1]),Vector3(rotation_m3[0][2],rotation_m3[1][2],rotation_m3[2][2])]
+	rotation_quaternion = mat3_normalized_to_quat_fast( rotation_m3);
+	
+	return {position=position,rotation_m3=rotation_m3,rotation_quaternion=rotation_quaternion,scale=scale}
+
+#void mat3_normalized_to_quat_fast(float q[4], const float mat[3][3])
+static func mat3_normalized_to_quat_fast(mat:Array):
+  #BLI_ASSERT_UNIT_M3(mat);
+  #/* Caller must ensure matrices aren't negative for valid results, see: #24291, #94231. */
+  #BLI_assert(!is_negative_m3(mat));
+
+  #/* Method outlined by Mike Day, ref: https://math.stackexchange.com/a/3183435/220949
+   #* with an additional `sqrtf(..)` for higher precision result.
+   #* Removing the `sqrt` causes tests to fail unless the precision is set to 1e-6 or larger. */
+	var q : Array = [0.0,0.0,0.0,0.0]
+	if (mat[2][2] < 0.0) :
+		if (mat[0][0] > mat[1][1]) :
+			var trace:float = 1.0 + mat[0][0] - mat[1][1] - mat[2][2];
+			var s:float = 2.0 * sqrt(trace);
+			if (mat[1][2] < mat[2][1]) :
+			#/* Ensure W is non-negative for a canonical result. */
+				s = -s;
+			
+			q[1] = 0.25 * s;
+			s = 1.0 / s;
+			q[0] = (mat[1][2] - mat[2][1]) * s;
+			q[2] = (mat[0][1] + mat[1][0]) * s;
+			q[3] = (mat[2][0] + mat[0][2]) * s;
+			#if (UNLIKELY((trace == 1.0f) && (q[0] == 0.0f && q[2] == 0.0f && q[3] == 0.0f))) {
+			if ((trace == 1.0) && (q[0] == 0.0 && q[2] == 0.0 && q[3] == 0.0)) :
+			#/* Avoids the need to normalize the degenerate case. */
+				q[1] = 1.0;
+	  
+		else :
+			var trace:float = 1.0 - mat[0][0] + mat[1][1] - mat[2][2];
+			var s:float = 2.0 * sqrt(trace);
+			if (mat[2][0] < mat[0][2]):
+				#/* Ensure W is non-negative for a canonical result. */
+				s = -s;
+			
+			q[2] = 0.25 * s;
+			s = 1.0 / s;
+			q[0] = (mat[2][0] - mat[0][2]) * s;
+			q[1] = (mat[0][1] + mat[1][0]) * s;
+			q[3] = (mat[1][2] + mat[2][1]) * s;
+			#if (UNLIKELY((trace == 1.0f) && (q[0] == 0.0f && q[1] == 0.0f && q[3] == 0.0f))) {
+			if ((trace == 1.0) && (q[0] == 0.0 && q[1] == 0.0 && q[3] == 0.0)) :
+			#/* Avoids the need to normalize the degenerate case. */
+				q[2] = 1.0;
+		
+	else :
+		if (mat[0][0] < -mat[1][1]) :
+			var trace:float = 1.0 - mat[0][0] - mat[1][1] + mat[2][2];
+			var s:float = 2.0 * sqrt(trace);
+			if (mat[0][1] < mat[1][0]):
+				#/* Ensure W is non-negative for a canonical result. */
+				s = -s;
+			
+			q[3] = 0.25 * s;
+			s = 1.0 / s;
+			q[0] = (mat[0][1] - mat[1][0]) * s;
+			q[1] = (mat[2][0] + mat[0][2]) * s;
+			q[2] = (mat[1][2] + mat[2][1]) * s;
+			#if (UNLIKELY((trace == 1.0f) && (q[0] == 0.0f && q[1] == 0.0f && q[2] == 0.0f))) {
+			if ((trace == 1.0) && (q[0] == 0.0 && q[1] == 0.0 && q[2] == 0.0)) :
+			#/* Avoids the need to normalize the degenerate case. */
+				q[3] = 1.0;
+	
+		else :
+		  #/* NOTE(@ideasman42): A zero matrix will fall through to this block,
+		   #* needed so a zero scaled matrices to return a quaternion without rotation, see: #101848. */
+			var trace:float = 1.0 + mat[0][0] + mat[1][1] + mat[2][2];
+			var s:float = 2.0 * sqrt(trace);
+			q[0] = 0.25 * s;
+			s = 1.0 / s;
+			q[1] = (mat[1][2] - mat[2][1]) * s;
+			q[2] = (mat[2][0] - mat[0][2]) * s;
+			q[3] = (mat[0][1] - mat[1][0]) * s;
+			#if (UNLIKELY((trace == 1.0f) && (q[1] == 0.0f && q[2] == 0.0f && q[3] == 0.0f))) {
+			if ((trace == 1.0) && (q[1] == 0.0 && q[2] == 0.0 && q[3] == 0.0)) :
+				#/* Avoids the need to normalize the degenerate case. */
+				q[0] = 1.0;
+
+	if (q[0] < 0.0):
+		printerr("q[0] is negative")
+
+  #/* Sometimes normalization is necessary due to round-off errors in the above
+   #* calculations. The comparison here uses tighter tolerances than
+   #* BLI_ASSERT_UNIT_QUAT(), so it's likely that even after a few more
+   #* transformations the quaternion will still be considered unit-ish. */
+
+	#const float q_len_squared = dot_qtqt(q, q);
+	#const float threshold = 0.0002f /* #BLI_ASSERT_UNIT_EPSILON */ * 3;
+	#if (fabs(q_len_squared - 1.0f) >= threshold) {
+		#normalize_qt(q);
+		
+	#var quat = Quaternion(q[0],q[1],q[2],q[3])
+	#quat = quat.normalized()
+	
+	return q
+
+
+#void mat3_to_rot_size(float rot[3][3], float size[3], const float mat3[3][3])
+static func mat3_to_rot_size(rot:Array, size:Vector3, mat3:Array):
+  #/* keep rot as a 3x3 matrix, the caller can convert into a quat or euler */
+	rot[0] = mat3[0].normalized()
+	rot[1] = mat3[1].normalized()
+	rot[2] = mat3[2].normalized()
+	#size[0] = mat3[0].dot(mat3[0]); I think?
+	#size[1] = normalize_v3_v3(rot[1], mat3[1]);
+	size[0] = 1
+	size[1] = 1
+	size[2] = 1
+	if (is_negative_m3(rot)) : #unlikely
+		printerr("Negative M3")
+		pass
+		#negate_m3(rot);
+		#negate_v3(size);
+	 
+#}
+
+static func is_negative_m3(mat:Array):
+	return determinant_m3_array(mat) < 0.0;
+
+static func determinant_m3_array(m:Array):
+	var determinant = (m[0][0] * (m[1][1] * m[2][2] - m[1][2] * m[2][1]) -
+		  m[1][0] * (m[0][1] * m[2][2] - m[0][2] * m[2][1]) +
+		  m[2][0] * (m[0][1] * m[1][2] - m[0][2] * m[1][1]));
+	#print(determinant)
+	return determinant
+
+#MINLINE float normalize_v3_v3_length(float r[3], const float a[3], const float unit_length)
+static func normalize_v3_v3_length(a:Vector3, unit_length:float)->float:
+	print("normalize_v3_v3_length")
+	var d:float = a.dot(a)
+	var r = Vector3.ZERO
+	#/* A larger value causes normalize errors in a scaled down models with camera extreme close. */
+	if (d > 1.0e-35):
+		d = sqrt(d)
+		r = mul_v3_v3fl(a, unit_length / d);
+		print(a)
+		print(r)
+	else:
+	#/* Either the vector is small or one of it's values contained `nan`. */
+		r.x = 0
+		r.y = 0
+		r.z = 0
+		d = 0.0;
+	#print(r)
+	return d;
+
+static func mul_v3_v3fl(a:Vector3, f:float):
+	#print("mul_v3_v3fl")
+	var r = Vector3.ZERO
+	r[0] = a[0] * f;
+	r[1] = a[1] * f;
+	r[2] = a[2] * f;
+	return r
+
+#void mul_m4db_m4db_m4fl(double R[4][4], const double A[4][4], const float B[4][4])
+static func mul_m4db_m4db_m4fl(A:Array, B:Array):
+  #if (R == A) {
+	#double T[4][4];
+	#mul_m4db_m4db_m4fl(T, A, B);
+	#copy_m4_m4_db(R, T);
+	#return;
+  #}
+
+  #/* Matrix product: `R[j][k] = B[j][i] . A[i][k]`. */
+	var R = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+	R[0][0] = B[0][0] * A[0][0] + B[0][1] * A[1][0] + B[0][2] * A[2][0] + B[0][3] * A[3][0];
+	R[0][1] = B[0][0] * A[0][1] + B[0][1] * A[1][1] + B[0][2] * A[2][1] + B[0][3] * A[3][1];
+	R[0][2] = B[0][0] * A[0][2] + B[0][1] * A[1][2] + B[0][2] * A[2][2] + B[0][3] * A[3][2];
+	R[0][3] = B[0][0] * A[0][3] + B[0][1] * A[1][3] + B[0][2] * A[2][3] + B[0][3] * A[3][3];
+
+	R[1][0] = B[1][0] * A[0][0] + B[1][1] * A[1][0] + B[1][2] * A[2][0] + B[1][3] * A[3][0];
+	R[1][1] = B[1][0] * A[0][1] + B[1][1] * A[1][1] + B[1][2] * A[2][1] + B[1][3] * A[3][1];
+	R[1][2] = B[1][0] * A[0][2] + B[1][1] * A[1][2] + B[1][2] * A[2][2] + B[1][3] * A[3][2];
+	R[1][3] = B[1][0] * A[0][3] + B[1][1] * A[1][3] + B[1][2] * A[2][3] + B[1][3] * A[3][3];
+
+	R[2][0] = B[2][0] * A[0][0] + B[2][1] * A[1][0] + B[2][2] * A[2][0] + B[2][3] * A[3][0];
+	R[2][1] = B[2][0] * A[0][1] + B[2][1] * A[1][1] + B[2][2] * A[2][1] + B[2][3] * A[3][1];
+	R[2][2] = B[2][0] * A[0][2] + B[2][1] * A[1][2] + B[2][2] * A[2][2] + B[2][3] * A[3][2];
+	R[2][3] = B[2][0] * A[0][3] + B[2][1] * A[1][3] + B[2][2] * A[2][3] + B[2][3] * A[3][3];
+
+	R[3][0] = B[3][0] * A[0][0] + B[3][1] * A[1][0] + B[3][2] * A[2][0] + B[3][3] * A[3][0];
+	R[3][1] = B[3][0] * A[0][1] + B[3][1] * A[1][1] + B[3][2] * A[2][1] + B[3][3] * A[3][1];
+	R[3][2] = B[3][0] * A[0][2] + B[3][1] * A[1][2] + B[3][2] * A[2][2] + B[3][3] * A[3][2];
+	R[3][3] = B[3][0] * A[0][3] + B[3][1] * A[1][3] + B[3][2] * A[2][3] + B[3][3] * A[3][3];
+	return R
+
+
+#void copy_m3_from_m4(float m1[3][3], const float m2[4][4])
+static func make_m3_from_m4(m2:Array):
+	var m1 = [Vector3.ZERO,Vector3.ZERO,Vector3.ZERO]
+	m1[0][0] = m2[0][0];
+	m1[0][1] = m2[0][1];
+	m1[0][2] = m2[0][2];
+
+	m1[1][0] = m2[1][0];
+	m1[1][1] = m2[1][1];
+	m1[1][2] = m2[1][2];
+
+	m1[2][0] = m2[2][0];
+	m1[2][1] = m2[2][1];
+	m1[2][2] = m2[2][2];
+	return m1
+
+#void vec_roll_to_mat3(const float vec[3], const float roll, float r_mat[3][3])
+static func vec_roll_to_mat3(vec:Vector3, roll:float):
+	#print(nor)
+	var rmat = vec_roll_to_mat3_normalized(vec.normalized(), roll);
+	return rmat
